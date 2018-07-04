6e18ff9c0d3b6bf7951180e87fbd30c28f0b0f54
pharra <typechasing@gmail.com>
2018-07-04 15:16:38 +0800

    rewrite ObjModel.ts for adjusting code style

--- src/component/ObjModel.ts
+++ src/component/ObjModel.ts
@@ -1,32 +1,32 @@
-import { float } from "babylonjs";
+import * as BABYLON from 'babylonjs';
 
-class ObjModel{
+class ObjModel {
     private ModelSrc: string;
     private ModelType: string;
-    private Scale: float;
-    constructor(src: string, tp: string, Sc: float){
+    private Scale: BABYLON.float;
+    constructor(src: string, tp: string, Sc: BABYLON.float) {
         this.ModelSrc = src;
         this.ModelType = tp;
         this.Scale = Sc;
     }
-    
-    public GetModelSrc(): string{
+
+    public GetModelSrc(): string {
         return this.ModelSrc;
     }
-    public GetModelType(): string{
+    public GetModelType(): string {
         return this.ModelType;
     }
 
-    public GetScale(): float{
+    public GetScale(): BABYLON.float {
         return this.Scale;
     }
-    public SetModelSrc(src: string): void{
+    public SetModelSrc(src: string): void {
         this.ModelSrc = src;
     }
-    public SetModelType(tp: string): void{
+    public SetModelType(tp: string): void {
         this.ModelType = tp;
     }
-    public SetScale(sc: float): void{
+    public SetScale(sc: BABYLON.float): void {
         this.Scale = sc;
     }
 }
