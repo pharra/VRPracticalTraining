a6e3e5d08f4f8fbe4c2b54e1c52054abdaf6a9b4
pharra <typechasing@gmail.com>
2018-07-05 17:29:44 +0800

    add toolbar in vue

---
+++ frontend/src/component/Toolbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <canvas id="toolbar"></canvas>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+export default class Toolbar extends Vue {
+    
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
