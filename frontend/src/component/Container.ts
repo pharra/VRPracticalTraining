11868b59fa28e60da319fe6c18c3dc4d2aea7828
pharra <typechasing@gmail.com>
2018-07-05 14:54:01 +0800

    add error log for load files

--- frontend/src/component/Container.ts
+++ frontend/src/component/Container.ts
@@ -39,11 +39,12 @@ class Container {
         '/static/objectModel/fps_q3_2/', 'fps_q3_2.babylon');
         meshTask.onSuccess = (task) => {
             console.log('load stone sucess');
-            task.loadedMeshes[0].position = BABYLON.Vector3.Zero();
+            console.log(meshTask.loadedParticleSystems);
+            // task.loadedMeshes[0].position = BABYLON.Vector3.Zero();
         };
-        meshTask.onError = (task) => {
-            console.log('load stone error');
-        };
+        meshTask.onError = (task, message, exception) => {
+            console.log(message, exception);
+        }
         assetsManager.load();
         return false;
     }
@@ -51,23 +52,14 @@ class Container {
 
     public createScene(): void {
         // Create a basic BJS Scene object.
-        this.scene = new BABYLON.Scene(this.engine);
+        this.arcRotateCamera = new BABYLON.ArcRotateCamera('camera', 0, 0, 10, BABYLON.Vector3.Zero(), this.scene);
 
-        // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
-        this.freeCamera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), this.scene);
-
-        // Target the camera to scene origin.
-        this.freeCamera.setTarget(BABYLON.Vector3.Zero());
 
         // Attach the camera to the canvas.
-        this.freeCamera.attachControl(this.canvas, false);
+        this.arcRotateCamera.attachControl(this.canvas, false);
 
         // Create a basic light, aiming 0,1,0 - meaning, to the sky.
         this.light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this.scene);
-
-        // Create a built-in "ground" shape.
-        const ground = BABYLON.MeshBuilder.CreateGround('ground',
-            { width: 6, height: 6, subdivisions: 2 }, this.scene);
     }
 
     public doRender(): void {
