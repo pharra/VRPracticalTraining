a305e2152737218775cb85f61ff889b24cf0df70
pharra <typechasing@gmail.com>
2018-07-16 17:54:16 +0800

    add entrypoint

--- vue.config.js
+++ vue.config.js
@@ -19,4 +19,43 @@ module.exports = {
         }
       }
     },
+    pages: {
+      home: {
+        // entry for the page
+        entry: 'src/home/home.js',
+        // the source template
+        template: 'public/home.html',
+        // output as dist/index.html
+        filename: 'home.html',
+        // when using title option,
+        // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+        title: 'home Page'
+      },
+      // when using the entry-only string format,
+      // template is inferred to be `public/subpage.html`
+      // and falls back to `public/index.html` if not found.
+      // Output filename is inferred to be `subpage.html`
+      index: {
+        // entry for the page
+        entry: 'src/main.ts',
+        // the source template
+        template: 'public/index.html',
+        // output as dist/index.html
+        filename: 'index.html',
+        // when using title option,
+        // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+        title: 'index Page'
+      },
+      login: {
+        // entry for the page
+        entry: 'src/login/login.js',
+        // the source template
+        template: 'public/Login.html',
+        // output as dist/index.html
+        filename: 'login.html',
+        // when using title option,
+        // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+        title: 'Login Page'
+      },
+    },
   }
