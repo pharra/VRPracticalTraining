34093fe043d28d4a00a3440219a8d47121955897
Anneyino <1132735216@qq.com>
2018-07-16 17:29:28 +0800

    NaviBar modify

--- src/component/Navibar.vue
+++ src/component/Navibar.vue
@@ -3,26 +3,49 @@
 			
 				<!-- Header -->
   <header class="header-area inner-page">
-	<!-- Header Top Area -->
-   <section class="header-top">
+	<nav class="navigation">
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-12 col-xs-12">
-					<div class="header-elements pull-right">
-						<div class="language pull-left">
-							<a href="/home">
-								<i class="icon-user"></i><span><h3>Home</h3></span>
-							</a>
-						</div>
-						<div class="login-or-signup pull-left">
-							<a href="/login"><i class="icon-user"></i><span><h3>Login/sign up</h3></span></a>
-						</div>
+					<div class="logo pull-left">
+						<a href="/home"><img alt="VR Practical" src="/webStatic/index/images/home1/logo.png" style="height: 45px;"></a> 
+					</div>
+					<ul class="main-menu pull-right">
+						<li><a href="/home">主页</a></li>
+						<li><a href="about-us.html">关于</a></li>
+						<li class="menu-has-child">
+							<a href="#">帮助</a>
+							<ul>
+								<li>
+									<a href="event.html">产品</a>
+									<ul>
+										<li><a href="event-details.html">产品细节</a></li>
+									</ul>
+								</li>
+								<li><a href="routine.html">用户指南</a></li>
+								<li><a href="contact.html">联系我们</a></li>
+							</ul>
+						</li>
+			
+							<li><a href="/login">登陆</a></li>
+							<li><a href="/login">注册</a></li>
+						
+						<li>
+							<a href="#" id="search-pop"><i class="fa fa-search"></i></a>
+						</li>
+					</ul>
+					<div class="mobile-menu hidden-lg hidden-md hidden-sm">
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
 					</div>
 				</div>
 			</div>
 		</div>
-	</section>
+	</nav>
 </header>
+
  </div>
 
 </template>
--- src/component/Toolbar.vue
+++ src/component/Toolbar.vue
@@ -1,25 +1,5 @@
 <template>
   <div id='toolbar'>
-
-		<!-- Page Wrapper -->
-			<div id='page-wrapper'>
-
-				<!-- Header -->
-
-				<!-- Banner -->
-					<section id='banner'>
-						<div class='inner'>
-							<h2>VR Practical</h2>
-							<p>一个全面的<br />
-							虚拟仿真<br />
-							野外实训平台<br /> <a href='#'>by Wuhan University</a>.</p>
-							<ul class='actions'>
-								<li><a href='#' class='button special'>开始模拟实训</a></li>
-							</ul>
-						</div>
-						<a href='#one' class='more scrolly'>Learn More</a>
-					</section>
-			</div>
   </div>
 </template>
 
